5056463ae1e4a9f2a998ca969bf591f62a8c9cdc
harrynnz <giahuyhuynh207@gmail.com>
2026-03-06 10:45:54 -0600

    update sapwnball demo

--- 04-ball-array/sketch.js
+++ 04-ball-array/sketch.js
@@ -17,30 +17,45 @@ function setup() {
 function draw() {
   background(220);
 
-
   for(let ball of ballArray) {
     ball.x += ball.dx;
     ball.y += ball.dy;
 
+    if (ball.x - ball.radius > width) {
+      ball.x -= width;
+    }
 
+    if (ball.x + ball.radius < 0) {
+      ball.x = width + ball.radius;
+    }
+    if (ball.y - ball.radius > height) {
+      ball.y -= height;
+    }
+    if (ball.y + ball.radius < 0) {
+      ball.y = height + ball.radius;
+    }
+    fill(ball.r, ball.g, ball.b);
     circle(ball.x, ball.y, ball.radius*2);
   }
 }
 
 
-
 function mousePressed() {
-  spawnBall();
+  spawnBall(mouseX, mouseY);
 }
 
-function spawnBall() {
-  let somebBall = {
-    x: random(width),
-    y: random(height),
+
+function spawnBall(_x, _y) {
+  let someBall = {
+    x: _x,
+    y: _y,
     dx: random(-5, 5),
     dy: random(-5, 5),
     radius: random(10, 30),
+    r: random(0, 255),
+    g: random(0, 255),
+    b: random(0, 255),
   };
 
-  ballArray.push(somebBall);
+  ballArray.push(someBall);
 }
